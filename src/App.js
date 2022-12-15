8e978c2e92bed90e0ec11a8c5592b1946b008a9c
Federico Krenn <fedekrenn@gmail.com>
2022-12-14 22:31:34 -0300

    Día 12, se agrega un campo destinatario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,109 @@
+import { useState } from 'react';
+
+import Modal from './components/modal/Modal';
+
+import Button from '@mui/material/Button';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
 function App() {
+
+  const [gifts, setGifts] = useState(JSON.parse(localStorage.getItem("gifts")) || []);
+
+  const [open, setOpen] = useState(false);
+  const [giftName, setGiftName] = useState('');
+  const [giftThumbnail, setGiftThumbnail] = useState('');
+  const [giftQuantity, setGiftQuantity] = useState('');
+  const [giftReceiver, setGiftReceiver] = useState('');
+
+  const addGift = () => {
+
+    if (giftName === "") return alert("El campo no puede estar vacío")
+
+    const existentName = gifts.some(gift => gift.name.trim().toLowerCase() === giftName.toLowerCase())
+
+    if (existentName) return alert("Ese regalo ya existe")
+
+    const idArr = gifts.map(item => item.id)
+    const maxId = idArr.length === 0 ? 0 : Math.max(...idArr)
+
+    const newGift = {
+      id: maxId + 1,
+      name: giftName,
+      quantity: giftQuantity,
+      thumbnail: giftThumbnail,
+      giftReceiver: giftReceiver
+    }
+
+    setGifts([...gifts, newGift])
+
+    localStorage.setItem("gifts", JSON.stringify([...gifts, newGift]))
+  }
+
+  const deleteGift = (id) => {
+
+    const filteredArr = gifts.filter(gift => gift.id !== id)
+
+    setGifts(filteredArr)
+
+    localStorage.setItem("gifts", JSON.stringify(filteredArr))
+  }
+
+  const deleteAll = () => {
+    setGifts([])
+    localStorage.setItem("gifts", "[]")
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleAdd = () => {
+    addGift()
+    handleClose();
+  }
+
   return (
-    <>
-    </>
+    <div className="box-section">
+      <h1>Regalos</h1>
+
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Agregar Regalos
+      </Button>
+
+      <Modal
+        handleAdd={handleAdd}
+        setGiftName={setGiftName}
+        setGiftThumbnail={setGiftThumbnail}
+        setGiftQuantity={setGiftQuantity}
+        open={open}
+        handleClose={handleClose}
+        setGiftReceiver={setGiftReceiver}
+      />
+
+      {gifts.length === 0 ?
+        <h4 className='gifts-container'>Agrega un relago!</h4>
+        :
+        <div>
+          <ul className="gifts-container">
+            {gifts.map((gift) => (
+              <li key={gift.id}>
+                <img className='thumb' src={gift.thumbnail} alt={gift.name} />
+                <div>
+                  <h4>{gift.name} {gift.quantity > 1 && `X ${gift.quantity}`}</h4>
+                  <p className='receiver'>{gift.giftReceiver}</p>
+                </div>
+                <DeleteForeverIcon className='icon' onClick={() => deleteGift(gift.id)} />
+              </li>
+            ))}
+          </ul>
+          <Button variant='outlined' onClick={deleteAll}>Eliminar todo</Button>
+        </div>
+      }
+    </div>
   );
 }
 
---
+++ src/components/modal/Modal.js
@@ -0,0 +1,29 @@
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const Modal = ({ handleAdd, setGiftName, setGiftThumbnail, setGiftQuantity, handleClose, open, setGiftReceiver }) => {
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Agregar un regalo</DialogTitle>
+            <DialogContent>
+                <form className='add-product-form'>
+                    <TextField label="Nombre del regalo" variant="standard" onChange={(e) => setGiftName(e.target.value)} required />
+                    <TextField type="url" label="Url de imagen" variant="standard" onChange={(e) => setGiftThumbnail(e.target.value)} required />
+                    <TextField type="number" label="Cantidad" variant="standard" onChange={(e) => setGiftQuantity(e.target.value)} required />
+                    <TextField label="Para quien es?" variant="standard" onChange={(e) => setGiftReceiver(e.target.value)} required />
+                </form>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleAdd}>Agregar</Button>
+                <Button onClick={handleClose}>Cancelar</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default Modal
